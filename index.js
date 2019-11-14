7dd3b000128c8fc98dc5965f516dc07f91d1a2dd
Billy Park <billyparkweb@gmail.com>
2019-11-14 13:15:58 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$(".imgLink").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+        'overlayShow'	:	false,
+        'hideScrollbar' : false
+	});
+	
+});
